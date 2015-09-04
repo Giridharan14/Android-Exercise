22137e01ddc680dbf5f37f890afdadb3539629b1
Giridharan A R <green.giri.re@gmail.com>
2015-09-04 19:22:20 +0530

    Day7

---
+++ Day7/Ex_14SMS Manager/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.t1;
+
+import java.util.ArrayList;
+
+import android.support.v7.app.ActionBarActivity;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+
+public class MainActivity extends ActionBarActivity  implements OnItemClickListener {
+
+    private static MainActivity inst;
+    ArrayList<String> smsMessagesList = new ArrayList<String>();
+    ListView smsListView;
+    ArrayAdapter arrayAdapter;
+
+    public static MainActivity instance() {
+        return inst;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        inst = this;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        smsListView = (ListView) findViewById(R.id.SMSList);
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, smsMessagesList);
+        smsListView.setAdapter(arrayAdapter);
+        smsListView.setOnItemClickListener(this);
+
+        refreshSmsInbox();
+    }
+
+    public void refreshSmsInbox() {
+        ContentResolver contentResolver = getContentResolver();
+        Cursor smsInboxCursor = contentResolver.query(Uri.parse("content://sms/inbox"), null, null, null, null);
+        int indexBody = smsInboxCursor.getColumnIndex("body");
+        int indexAddress = smsInboxCursor.getColumnIndex("address");
+        if (indexBody < 0 || !smsInboxCursor.moveToFirst()) return;
+        arrayAdapter.clear();
+        do {
+            String str = "SMS From: " + smsInboxCursor.getString(indexAddress) +
+                    "\n" + smsInboxCursor.getString(indexBody) + "\n";
+            arrayAdapter.add(str);
+        } while (smsInboxCursor.moveToNext());
+    }
+
+    public void updateList(final String smsMessage) {
+        arrayAdapter.insert(smsMessage, 0);
+        arrayAdapter.notifyDataSetChanged();
+    }
+
+    public void onItemClick(AdapterView<?> parent, View view, int pos, long id) {
+        try {
+            String[] smsMessages = smsMessagesList.get(pos).split("\n");
+            String address = smsMessages[0];
+            String smsMessage = "";
+            for (int i = 1; i < smsMessages.length; ++i) {
+                smsMessage += smsMessages[i];
+            }
+
+            String smsMessageStr = address + "\n";
+            smsMessageStr += smsMessage;
+            Toast.makeText(this, smsMessageStr, Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Day7/Ex_14SMS Manager/SmsBroadcastReceiver.java
@@ -0,0 +1,35 @@
+package com.example.t1;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+public class SmsBroadcastReceiver extends BroadcastReceiver {
+
+    public static final String SMS_BUNDLE = "pdus";
+
+    public void onReceive(Context context, Intent intent) {
+        Bundle intentExtras = intent.getExtras();
+        if (intentExtras != null) {
+            Object[] sms = (Object[]) intentExtras.get(SMS_BUNDLE);
+            String smsMessageStr = "";
+            for (int i = 0; i < sms.length; ++i) {
+                SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) sms[i]);
+
+                String smsBody = smsMessage.getMessageBody().toString();
+                String address = smsMessage.getOriginatingAddress();
+
+                smsMessageStr += "SMS From: " + address + "\n";
+                smsMessageStr += smsBody + "\n";
+            }
+            Toast.makeText(context, smsMessageStr, Toast.LENGTH_SHORT).show();
+
+            //this will update the UI with message
+            MainActivity inst = MainActivity.instance();
+            inst.updateList(smsMessageStr);
+        }
+    }
+}
